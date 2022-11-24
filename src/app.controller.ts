c3ad9ee510e03e1be73e7c71ccaf15b7e1519e4b
Joud Awad <joud.awad@metalabdesign.com>
2022-11-24 21:03:19 +0300

    removing prisma

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,24 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
-import { User } from '@prisma/client';
 import { AppService } from './app.service';
-import { PrismaService } from './services/prisma/prisma.service';
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    private readonly prismaService: PrismaService,
-  ) {}
+  constructor(private readonly appService: AppService) {}
 
   @Get()
   async getHello(): Promise<string> {
-    const random = Math.round(Math.random() * 10e7);
-    const user = await this.prismaService.user.create({
-      data: {
-        email: `email-${random}@gmail.com`,
-        firebaseId: `firebase-${random}`,
-      },
-    });
-    return this.appService.getHello(user);
+    return this.appService.getHello();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { User } from '@prisma/client';
 
 @Injectable()
 export class AppService {
-  getHello(user: User) {
+  getHello() {
     return 'hello World';
   }
 }
