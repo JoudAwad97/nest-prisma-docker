5ec994c4851930dd26ae639128ca04418177382f
Joud Awad <joud.awad@metalabdesign.com>
2022-11-24 21:03:56 +0300

    updaing the response

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello() {
-    return 'hello World';
+    return 'DATABASE_URL:' + process.env.DATABASE_URL;
   }
 }
