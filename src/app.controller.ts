c44f7ae57e2211a5212dc9890f528f418fa7ac4d
Joud Awad <joud.awad@metalabdesign.com>
2022-11-26 00:22:24 +0300

    testing build with no prisma

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,23 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
-import { User } from '@prisma/client';
+// import { User } from '@prisma/client';
 import { AppService } from './app.service';
-import { PrismaService } from './services/prisma/prisma.service';
+// import { PrismaService } from './services/prisma/prisma.service';
 
 @Controller()
 export class AppController {
   constructor(
-    private readonly appService: AppService,
-    private readonly prismaService: PrismaService,
+    private readonly appService: AppService, //private readonly prismaService: PrismaService,
   ) {}
 
   @Get()
-  async getHello(): Promise<User> {
+  async getHello(): Promise<string> {
     const random = Math.floor(Math.random() * 10e7);
-    return await this.prismaService.user.create({
-      data: {
-        email: `test-${random}@gmail.com`,
-        firebaseId: `firebase-${random}`,
-      },
-    });
+    return 'Hello World';
   }
 }
